c1aebd81d403880a21ca9b6a05f0226f005a844c
HannaBodnarovska <bodnarovska2000@gmail.com>
2023-11-05 12:59:32 +0100

    new new

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,16 +5,13 @@ const Statistics = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  useEffect(() => {
-    const total = good + neutral + bad;
-    const positivePercentage = total === 0 ? 0 : (good / total) * 100;
-    setTotal(total);
-    setPositivePercentage(positivePercentage);
-  }, [good, neutral, bad]);
-
   const [total, setTotal] = useState(0);
   const [positivePercentage, setPositivePercentage] = useState(0);
+
+  useEffect(() => {
+    setTotal(good + neutral + bad);
+    setPositivePercentage(total === 0 ? 0 : (good / total) * 100);
+  }, [good, neutral, bad, total]);
 
   return (
     <div className={styles.container}>
@@ -28,3 +25,4 @@ const Statistics = () => {
 };
 
 export default Statistics;
+
